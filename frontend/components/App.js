b5d9a6f06d9188bf681ef01206a6685e0fd8a35d
Sydney <proteimus@gmail.com>
2024-02-02 09:53:11 -0600

    Houston, we're talking OFF 🚀

---
+++ frontend/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  const num = 7
+  return (
+    <>
+    <div id="container">
+      <h2 className="big">Hello, World!</h2>
+    </div>
+      <div id="container2">
+        <h2 className="big">{num}</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
